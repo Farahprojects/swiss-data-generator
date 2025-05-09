f6d446215787c6452b0cdd56b0acb141a3b3ad51
Farahprojects <peter@farahprojects.com.au>
2025-05-10 08:50:03 +1000

    Update balanceChecker.ts

--- supabase/functions/_shared/balanceChecker.ts
+++ supabase/functions/_shared/balanceChecker.ts
@@ -2,7 +2,7 @@
 import { createClient, SupabaseClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 const supabaseUrl = Deno.env.get("SUPABASE_URL")!;
-const supabaseServiceKey = Deno.env.get("SERVICE_ROLE_KEY")!;
+const supabaseServiceKey = Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")!;
 const sb: SupabaseClient = createClient(supabaseUrl, supabaseServiceKey);
 
 export interface BalanceCheckResult {
