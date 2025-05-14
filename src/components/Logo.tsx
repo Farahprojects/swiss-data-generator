c3bbb68c92a43ffcc5a38328312721b3d666a6f9
Farahprojects <peter@farahprojects.com.au>
2025-05-15 05:07:02 +1000

    Update Logo.tsx

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -44,7 +44,7 @@ const Logo = ({ className, linkClassName, size = 'md', asLink = true }: LogoProp
           sizeClasses[size]
         )}
       >
-        Swiss
+        Astro
       </span>
     </div>
   );
