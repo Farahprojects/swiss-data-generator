bbdf370d3ab86326b4c33ead6551992058ef5051
Farahprojects <peter@farahprojects.com.au>
2025-05-25 22:23:38 +1000

    Update index.ts

--- supabase/functions/email-verification/index.ts
+++ supabase/functions/email-verification/index.ts
@@ -1,5 +1,5 @@
 
-// deno-lint-ignore-file no-explicit-any1
+// deno-lint-ignore-file no-explicit-any10
 import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
