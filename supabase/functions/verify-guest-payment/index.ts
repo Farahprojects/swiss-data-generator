08f6bc740503e5c23ed01988736a9e40e0973f74
Farahprojects <peter@farahprojects.com.au>
2025-07-29 12:16:42 +1000

    Update index.ts

--- supabase/functions/verify-guest-payment/index.ts
+++ supabase/functions/verify-guest-payment/index.ts
@@ -1,4 +1,4 @@
-// latest vesrion 
+// latest vesrion 29th
 import { serve } from "https://deno.land/std@0.224.0/http/server.ts";
 import Stripe from "https://esm.sh/stripe@14?target=denonext";
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2?target=deno&deno-std=0.224.0";
