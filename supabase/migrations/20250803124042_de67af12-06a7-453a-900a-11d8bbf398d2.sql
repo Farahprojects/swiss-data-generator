0e6a5e3acdaa6782e84e8b4a6a1bcb1bd0e42d51
Farahprojects <peter@farahprojects.com.au>
2025-08-04 11:12:23 +1000

    Remove problematic migration files and clean up merge state

--- supabase/migrations/20250803124042_de67af12-06a7-453a-900a-11d8bbf398d2.sql
+++
@@ -1,5 +0,0 @@
--- Create trigger on report_logs to link back to guest_reports
-CREATE TRIGGER after_insert_report_log
-  AFTER INSERT ON public.report_logs
-  FOR EACH ROW
-  EXECUTE FUNCTION public.link_report_log_to_guest();
