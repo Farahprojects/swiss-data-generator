a6ee5f93fc82e2ce8e81f6743149823bc42dcf1b
Farahprojects <peter@farahprojects.com.au>
2025-08-07 11:33:13 +1000

    Remove SuccessScreen from mobile drawer - unwrapped

--- src/components/public-report/MobileReportDrawer.tsx
+++ src/components/public-report/MobileReportDrawer.tsx
@@ -297,34 +297,11 @@ const MobileReportDrawer: React.FC<MobileReportDrawerProps> = ({
     onOpenChange(false);
   };
 
-  // Show success screen if report was created
-  if (reportCreated && userName && userEmail) {
-    const guestReportId = localStorage.getItem('currentGuestReportId');
-    return (
-      <Drawer.Root open={isOpen} onOpenChange={onOpenChange}>
-        <Drawer.Portal>
-          <Drawer.Overlay className="fixed inset-0 bg-black/40 z-[100]" />
-          <Drawer.Content className="fixed bottom-0 left-0 right-0 h-[96%] bg-white rounded-t-2xl z-[100] overflow-hidden">
-            <div className="h-full overflow-y-auto">
-              <MobileDrawerProvider closeDrawer={() => onOpenChange(false)}>
-                <SuccessScreen 
-                  name={userName} 
-                  email={userEmail} 
-                  guestReportId={guestReportId || undefined}
-                />
-              </MobileDrawerProvider>
-            </div>
-          </Drawer.Content>
-        </Drawer.Portal>
-      </Drawer.Root>
-    );
-  }
-
   return (
     <Drawer.Root open={isOpen} onOpenChange={onOpenChange}>
       <Drawer.Portal>
           <Drawer.Overlay className="fixed inset-0 bg-white z-[100]" />
-        <Drawer.Content className="fixed bottom-0 left-0 right-0 h-[96%] bg-white rounded-t-2xl z-[100] flex flex-col">
+        <Drawer.Content className="fixed bottom-0 left-0 right-0 h-[96%] bg-white rounded-2xl z-[100] flex flex-col">
           
           {/* Header */}
           <MobileDrawerHeader
