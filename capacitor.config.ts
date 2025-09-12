555c8dd63e42902aa085b143d3069a13e75a1622
Farahprojects <peter@farahprojects.com.au>
2025-09-12 10:59:28 +1000

    Branding: rename app to 'Therai' for iOS and Android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.farahprojects.therai',
-  appName: 'therai-co',
+  appName: 'Therai',
   webDir: 'dist'
 };
 
