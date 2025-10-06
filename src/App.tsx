1b6f40a71bc501c7888f3464979c75b8cdfd11fe
Farahprojects <peter@farahprojects.com.au>
2025-10-06 22:27:58 +1100

    Remove unused JoinConversation feature causing 400 errors

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Toaster } from 'sonner'
 import AuthedAppShell from '@/AuthedAppShell'
-import JoinConversation from '@/pages/JoinConversation'
 import { AuthProvider } from '@/contexts/AuthContext'
 import NavigationStateProvider from '@/contexts/NavigationStateContext'
 import { ModeProvider } from '@/contexts/ModeContext'
@@ -49,10 +48,7 @@ function App() {
       <Router>
         <AppProviders>
           <Routes>
-            {/* Public routes - no auth required */}
-            <Route path="/join/:chatId" element={<JoinConversation />} />
-            
-            {/* All other routes go through AuthedAppShell */}
+            {/* All routes go through AuthedAppShell */}
             <Route path="/*" element={<AuthedAppShell />} />
           </Routes>
         </AppProviders>
